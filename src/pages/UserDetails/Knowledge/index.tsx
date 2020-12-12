ecb1945f0802b8eb576c9c8227b56755fb4d31c3
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2020-12-12 18:36:47 -0300

    Add a popup

--- src/pages/UserDetails/Knowledge/index.tsx
+++ src/pages/UserDetails/Knowledge/index.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Label } from '../../../components/Forms';
+import Popup from '../../../components/Popup';
 import Progress from '../../../components/Progress';
 import UserSection from '../UserSection';
 import { KnowledgeGroup } from './styles';
 
 const Knowledge: React.FC = () => {
   return (
-    <UserSection title="Conhecimentos" onEdit={() => {}}>
-      <KnowledgeGroup>
-        <Label> Javascript </Label>
-        <Progress percent={80}/>
-      </KnowledgeGroup>
-      <KnowledgeGroup>
-        <Label> Javascript </Label>
-        <Progress percent={70}/>
-      </KnowledgeGroup>
-      <KnowledgeGroup>
-        <Label> Javascript </Label>
-        <Progress percent={75}/>
-      </KnowledgeGroup>
-    </UserSection>
+    <>
+      <Popup isOpen={true} onRequestClose={() => {}}>
+          efwe
+      </Popup>
+      <UserSection title="Conhecimentos" onEdit={() => {}}>
+        <KnowledgeGroup>
+          <Label> Javascript </Label>
+          <Progress percent={80}/>
+        </KnowledgeGroup>
+        <KnowledgeGroup>
+          <Label> Javascript </Label>
+          <Progress percent={70}/>
+        </KnowledgeGroup>
+        <KnowledgeGroup>
+          <Label> Javascript </Label>
+          <Progress percent={75}/>
+        </KnowledgeGroup>
+      </UserSection>
+    </>
   )
 };
 
