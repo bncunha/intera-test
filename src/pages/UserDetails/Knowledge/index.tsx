04a11999e7ca163b7d58d3391293cd35338a7ff0
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2020-12-13 11:32:47 -0300

    Add knowledge vaildation and save data

--- src/pages/UserDetails/Knowledge/index.tsx
+++ src/pages/UserDetails/Knowledge/index.tsx
@@ -1,6 +1,7 @@
-import { Scope } from '@unform/core';
+import { FormHandles, Scope } from '@unform/core';
 import { Form } from '@unform/web';
-import React, { useState } from 'react';
+import { type } from 'os';
+import React, { useRef, useState } from 'react';
 import { FaMinusSquare, FaPlusSquare } from 'react-icons/fa';
 import { Label } from '../../../components/Forms';
 import { Button, ButtonsGroup } from '../../../components/Forms/Button';
@@ -12,22 +13,48 @@ import { Title2 } from '../../../components/Texts';
 import { MinusButton, PlusButton } from '../About/styles';
 import UserSection from '../UserSection';
 import { KnowledgeGroup } from './styles';
+import * as yup from 'yup';
+import getValidationErrors from '../../../utils/getValidationErros';
+import { KnowledgeSchema } from './schema';
+import { UsuarioService } from '../../../services/UsuarioService';
+import { Conhecimento } from '../../../services/api';
 
 const Knowledge: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [qtdItems, setQtdItems] = useState<number>(1);
 
+  const handleSubmit = async (data: {conhecimentos?: Conhecimento[]}) => {
+    try {
+      if (Object.keys(data).length === 0) {
+        data.conhecimentos = [];
+      }
+      formRef.current?.setErrors({});
+      const schema: any = await KnowledgeSchema.validate(data, {abortEarly: false});
+      UsuarioService.saveUsuario('Bruno Cunha', schema);
+    } catch (err) {
+      if (err instanceof yup.ValidationError) {
+        formRef.current?.setErrors(getValidationErrors(err))
+      }
+    }
+  };
+
+  const handleCancel = () => {
+    setIsOpen(false);
+    formRef.current?.setErrors({});
+  }
+
   const renderFormPopup = () => (
-    <Popup isOpen={isOpen} onRequestClose={setIsOpen}>
+    <Popup isOpen={isOpen} onRequestClose={handleCancel}>
       <Row noWrap style={{alignItems: 'center'}}>
         <Title2 style={{fontSize: 18}}> Conhecimentos </Title2>
         <PlusButton type="button" onClick={() => setQtdItems(qtdItems + 1)}> <FaPlusSquare/> </PlusButton>
       </Row>
-      <Form onSubmit={(d) => console.log(d)}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         {
           [...Array(qtdItems)].map((value: any, index: number) => (
             <Scope path={`conhecimentos[${index}]`} key={index}>
-              <Row noWrap style={{alignItems: 'center', marginBottom: 10}}>
+              <Row noWrap style={{marginBottom: 10}}>
                 <div>
                   <Label> Nome </Label>
                   <Input name="nome"/>
@@ -48,7 +75,7 @@ const Knowledge: React.FC = () => {
         }
         <ButtonsGroup>
           <Button type="submit"> Salvar </Button>
-          <Button type="button" marginLeft="5px" template="secondary" onClick={() => setIsOpen(false)}> Cancelar </Button>
+          <Button type="button" marginLeft="5px" template="secondary" onClick={handleCancel}> Cancelar </Button>
         </ButtonsGroup>
       </Form>
     </Popup>
