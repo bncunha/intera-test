81b565df456720aefa0a22683024c279a4bb6cb7
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2020-12-13 18:17:45 -0300

    Exibir e filtrar usuarios por nome

--- src/pages/UserList/index.tsx
+++ src/pages/UserList/index.tsx
@@ -1,23 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {  Main, Row } from '../../components/Layout';
-import { Title1, Title2 } from '../../components/Texts';
 import { Usuario } from '../../services/api';
 import { UsuarioService } from '../../services/UsuarioService';
-import { UserCardContainer } from './styles';
+import { UserCardContainer, UserSearch } from './styles';
 import UserCard from './UserCard';
 
 const UserList: React.FC = () => {
   const [users, setUsers] = useState<Usuario[]>([]);
+  const [usersFiltered, setUsersFiltered] = useState<Usuario[]>([]);
+
+  const filterByName = useCallback((name: string) => {
+    console.log(name)
+    setUsersFiltered(
+      name 
+      ? users.filter(user => user.nome?.toUpperCase().indexOf(name.toUpperCase()) >= 0)
+      : users);
+  }, [users])
 
   useEffect(() => {
-    setUsers(UsuarioService.getAll())
+    const allUsers = UsuarioService.getAll();
+    setUsers(allUsers);
+    setUsersFiltered(allUsers);
   }, []);
 
   return (
     <Main style={{paddingTop: 10}}>
+      <UserSearch placeholder="Buscar por talentos..." onChange={(ev) => filterByName(ev.target.value)}/>
       <Row style={{justifyContent: 'center'}}>
         {
-          users.map((user) => (
+          usersFiltered.map((user) => (
             <UserCardContainer>
               <UserCard key={user.nome} user={user}/>
             </UserCardContainer>
--- src/pages/UserList/styles.ts
+++ src/pages/UserList/styles.ts
@@ -1,6 +1,23 @@
 import styled from 'styled-components';
+import colors from '../../styles/colors';
 
 export const UserCardContainer = styled.div`
   flex-basis: 300px;
   margin: 15px;
 `;
+
+export const UserSearch = styled.input`
+  border: none;
+  color: ${colors.text};
+  border-bottom: 1px solid ${colors.text};
+  background: transparent;
+  border-radius: 0;
+  padding: 24px;
+  max-width: 600px;
+  width: 100%;
+  margin: auto;
+  display: block;
+  margin-bottom: 30px;
+  font-size: 23px;
+  text-align: center;
+`; 
